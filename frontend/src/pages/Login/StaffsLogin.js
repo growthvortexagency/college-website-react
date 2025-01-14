8a58e176212673e6359509c39e2463e350f878c5
growthvortexagency <growthvortexagency@gmail.com>
2025-01-15 01:16:53 +0530

    Front-End Staff Login Page Designed (Updated as on Jan 15 at 1: 17 AM)

--- frontend/src/pages/Login/StaffsLogin.js
+++ frontend/src/pages/Login/StaffsLogin.js
@@ -1,30 +1,76 @@
-import React from 'react'
-import '../../styles/StaffsLogin.css'
+import '../../styles/StaffsLogin.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const StaffsLogin = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div className='login-container'>
-      <div class="container1">
-        <div class="form-container sign-in-container">
-          <form action="">
-            <h1>Sign in</h1>
-            <input type="loginID" placeholder="Enter your login ID" name='loginID'/>
-            <input type="password" placeholder="Enter your Password" name='password'/>
-            <a href="#">Forgot your password?</a>
-            <button type='submit' >Sign In</button>
+    <div className='body'>
+      <div className={`container1 ${isActive ? 'active' : ''}`}>
+        <div className='form-box login'>
+          <form action=''>
+            <h1>Sign In</h1>
+            <div className='input-box'>
+              <input type='text' placeholder='Enter your User ID' required />
+              <FontAwesomeIcon className='icon' icon={faUser} />
+            </div>
+            <div className='input-box'>
+              <input type='password' placeholder='Enter your Password' required />
+              <FontAwesomeIcon className='icon' icon={faLock} />
+            </div>
+            <div className='forgot-link'>
+              <a>Forgot Password?</a>
+            </div>
+            <button type='submit' className='btn'>Sign In</button>
           </form>
         </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-right">
-              <h1>Hello!!</h1>
-              <p>Enter your login credentials to login to your account.</p>
+
+        <div className='form-box signup'>
+          <form action=''>
+            <h1>Sign Up</h1>
+            <div className='input-box'>
+              <input type='email' placeholder='Enter your Email' required />
+              <FontAwesomeIcon className='icon' icon={faEnvelope} />
             </div>
+            <div className='input-box'>
+              <input type='password' placeholder='Enter your Password' required />
+              <FontAwesomeIcon className='icon' icon={faLock} />
+            </div>
+            <button type='submit' className='btn'>Sign Up</button>
+          </form>
+        </div>
+
+        <div className='toggle-box'>
+          <div className='toggle-panel toggle-left'>
+            <h1>Welcome Back!!</h1>
+            <p>Enter your login credentials to sign-in to your account.</p>
+            <p>Don't have an account?</p>
+            <button className='btn register-btn' onClick={handleRegisterClick}>
+              Sign Up
+            </button>
+          </div>
+          <div className='toggle-panel toggle-right'>
+            <h1>Welcome</h1>
+            <p>Enter your Email and Password to create an account.</p>
+            <p>Already have an account?</p>
+            <button className='btn login-btn' onClick={handleLoginClick}>
+              Sign In
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StaffsLogin
+export default StaffsLogin;
